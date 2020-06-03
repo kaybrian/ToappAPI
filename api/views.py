31e88cbe6d4de11301c0d50a7584e775c4d2eb47
kaybrian <kayonbrian@gmail.com>
2020-06-03 12:56:20 +0300

    added a task serializer

--- api/views.py
+++ api/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
 # Create your views here.
+@api_view(['GET'])
 def apiOverView(request):
-    return JsonResponse('THis is the main page ',safe=False)
+    api_urls = {
+        'List':'task-list',
+        'Detail view':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/taksk-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+
+
+    return Response(api_urls)
