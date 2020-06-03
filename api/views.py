b843c3a88aa682d9efcc17f40f3965fdf28c89ad
kaybrian <kayonbrian@gmail.com>
2020-06-03 12:41:12 +0300

    the project setup is done

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+def apiOverView(request):
+    return JsonResponse('THis is the main page ',safe=False)
